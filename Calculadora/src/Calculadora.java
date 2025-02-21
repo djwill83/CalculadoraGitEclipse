afaee8e8f2708f87662d3cc690d6670639cf41be
guillermotormoperis <guillermotormoperis@mbpdeguillermo.lan>
2025-02-21 13:55:13 +0100

    Funciones de resta y multiplicación añadidas

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -3,6 +3,15 @@ public class Calculadora {
 		return a + b;
 	}
 
+	public int restar(int a, int b) {
+		return a - b;
+
+	}
+
+	public int multiplicar(int a, int b) {
+		return a * b;
+	}
+
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
 		System.out.println("Resultado de la suma: " + calc.sumar(5, 3));
